60efd523976c91d8d3d2511569e96e6a4a1f098e
bersim-8 <48282727+SimonBerghem@users.noreply.github.com>
2020-11-11 14:43:10 +0000

    init

---
+++ test.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
+@app.route("/Kenobi")
+def kenobi():
+    return "General Kenobi"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
