507ac53ca0f55a1f96af67f84ab4b922668423dd
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2020-11-11 16:04:13 +0100

    test if changes

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return "<h1 style='color:red'>Hello There!</h1>"
 
 @app.route("/Kenobi")
 def kenobi():
