e539c4b8edce92a92f1c2332f2387b84b22a7d61
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2020-11-18 09:25:48 +0100

    testing pymysql

--- test.py
+++ test.py
@@ -1,12 +1,29 @@
 from flask import Flask, request,render_template
-import psycopg2
-#try: 
-conn = psycopg2.connect(database="D0018E", user="bersim-8",  
-    password=" *210490214DCFD2AB2F8FD675AFBDE761AF56E941", host="127.0.0.1", port = "3306")
-    #print("connected")
-#except:
-    #print ("I am unable to connect to the database")
-mycursor = conn.cursor()
+import pymysql.cursors
+
+def execute(sql, isSelect = True):
+    conn = pymysql.connect(host='127.0.0.1',
+                           port=3306,
+                           user='bersim-8',
+                           password='*210490214DCFD2AB2F8FD675AFBDE761AF56E941',
+                           db ='D0018E',
+                           charset='utf8mb4',
+                           cursorclass=pymysql.cursors.DictCursor)
+    result = None
+    try:
+        with conn.cursor() as cursor:
+            if isSelect:
+                cursor.execute(sql)
+                result = cursor.fetchall()
+                print(f"result = {result}")
+            else:
+                cursor.execute(sql)
+                result = conn.insert_id()
+                conn.commit()
+    finally:
+        conn.close()
+    return result
+
 app = Flask(__name__)
 
 @app.route("/")
