cd38ba9876021733e34a5ce90c72329cd00a448a
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2020-11-24 09:59:42 +0100

    testing text box 4

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ def hello():
 @app.route('/', methods=['POST'])
 def my_form_post():
     text = request.form['text']
+    print(type(text))
     processed_text = text.upper()
     sql = ("INSERT INTO D0018E.Product (PID, PName, PPrice, PStock, PColor, PDescript, PRating) VALUES", text)
     execute(sql, False)
