bfb6e8e928e603c8b1c7d4e8d0c206133d6cefcd
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2020-11-24 16:07:58 +0100

    testing text box 45

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def my_form_post():
         print(update)
         update = tuple(update.split(", "))
         print("0 = ", update[0],"1 = ", update[1], "2 = ", update[2])
-        sql = ("UPDATE D0018E.Product SET PPrice = {1}, PStock= {2} WHERE PID = {0}".format(update)) 
+        sql = ("UPDATE D0018E.Product SET PPrice = {1} WHERE PID = {0}".format(update)) 
         res = execute(sql, False)
         print("x")
         
