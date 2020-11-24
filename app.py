05c87e1027f081340775b6abd1f2c9248b3767d9
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2020-11-24 10:41:47 +0100

    testing text box 14

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ It contains the definition of routes and views for the application.
 from flask import Flask, request,render_template
 import pymysql.cursors
 
-def execute(sql, isSelect = True, t = None):
+def execute(sql, isSelect = True):
     conn = pymysql.connect(host='127.0.0.1',
                            port=3306,
                            user='bersim-8',
@@ -45,8 +45,8 @@ def my_form_post():
     text = (4, "EPA4", 5555, 7, "SAAB", "En gammal EPA", 4)
     print(text)
     print(type(text))
-    sql = ("INSERT INTO `D0018E.Product` (`PID`, `PName`, `PPrice`, `PStock`, `PColor`, `PDescript`, `PRating`) VALUES (%d, %s, %d, %d, %s, %s, %d")
-    insert = execute(sql, False, text)
+    sql = ("INSERT INTO `D0018E.Product` (`PID`, `PName`, `PPrice`, `PStock`, `PColor`, `PDescript`, `PRating`) VALUES (%d, %s, %d, %d, %s, %s, %d)" % text)
+    insert = execute(sql, False)
     print(insert)
     sql = "Select PName, PPrice from D0018E.Product"
     data = execute(sql)
