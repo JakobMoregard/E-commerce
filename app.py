222fbacd3e42eb18b9ed808e63cad7f21c181636
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2020-11-25 13:00:18 +0100

    testing multiple inputs 18

--- app.py
+++ app.py
@@ -31,6 +31,8 @@ def execute(sql, isSelect = True):
 
 def parse_product_data(data, keys):
 
+    data_content = []
+
     for i in range(1, len(data)-1):
         if data[keys[i]] != "":
             data_content.append(data[keys[i]])
