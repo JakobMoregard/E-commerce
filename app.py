4d556fe8e0499f049eab9c1740ef65e03bf347e5
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2020-12-01 09:54:57 +0100

    cookies 8

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def hello():
     session = random.randint(0, 10000)
     res = make_response("Setting a cookie")
     print(session, str(session))
-    res.set_cookie('SID', '{}'.format(session), max_age=60*60*24*365*2)
+    res.set_cookie('SID', 'test', max_age=60*60*24*365*2)
 
     name = request.cookies.get('SID')
     print(name)
