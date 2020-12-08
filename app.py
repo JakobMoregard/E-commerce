ae171c5ec59f6387a94b5ddd40170d47a72253d1
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2020-12-08 15:15:57 +0100

    new file for parsing

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ It contains the definition of routes and views for the application.
 from flask import Flask, request,render_template, make_response, redirect, url_for
 import pymysql.cursors
 import random
+import parsing
 
 def execute(sql, isSelect = True):
     conn = pymysql.connect(host='127.0.0.1',
@@ -28,64 +29,6 @@ def execute(sql, isSelect = True):
     finally:
         conn.close()
     return result
-
-
-def parse_product_data(data, keys):
-
-    data_content = []
-    used_keys = []
-
-    for i in range(0, len(keys)):
-        print(data[keys[i]])
-        if data[keys[i]] != '':
-            data_content.append(data[keys[i]])
-        else:
-            used_keys.append(keys[i])
-            continue
-
-    #new_data = [tuple(keys), tuple(data_content)]
-    for j in range(0, len(used_keys)):    
-        keys.remove(used_keys[j])
-    print("parse ",  keys)
-    return data_content
-
-
-def parse_update_string(data, keys):
-
-    parse_string = ""
-
-    print(keys)
-
-    for i in range(1, len(keys)):
-        if i > 1:
-            parse_string += ","
-        print("key = " + keys[i] + " data = " + data[i])
-        parse_string += keys[i] + " = '" + data[i] + "'"
-        
-    
-    print(parse_string)
-    return parse_string
-
-def parse_registered_data(ID, data, keys):
-
-    data_content = []
-    used_keys = []
-
-    data_content.append(ID)
-    print(ID)
-
-    for i in range(1, len(keys)):
-        print(data[keys[i]])
-        if data[keys[i]] != '':
-            data_content.append(data[keys[i]])
-        else:
-            used_keys.append(keys[i])
-            continue
-    
-    for j in range(0, len(used_keys)):    
-        keys.remove(used_keys[j])
-    print("parse ",  keys)
-    return data_content
 
 
 
