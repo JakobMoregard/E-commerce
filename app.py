240e0f71b1941cbd203f87a6538afe8afc5521d7
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2020-12-09 16:43:01 +0100

    customer signup 22

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def cart_route():
 
     cookie_id = int(request.cookies.get('SID'))
 
-    if request.cookies.get('login') == 'None' and request.cookies.get('login') not in customers:
+    if request.cookies.get('login') == 'None' and request.cookies.get('SID') not in customers:
         res = make_response(redirect("/customer"))
         return res
 
