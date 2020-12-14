9ff3efdc73bf7058c5c8f01c40ca2ec1aaca1e56
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2020-12-14 19:05:57 +0100

    send id, amount 10

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def cart_route():
         if customers[i]['CID'] == cookie_id:
             flag = False
 
-    print(request.form['form_id'])
+    print(request.form['Amount'])
     data2 = "{0}, {1}".format(request.form['form_id'], request.form['Amount'])
     print(data2)
     if request.cookies.get('login') == 'None' and flag:
