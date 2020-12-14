816e9359afc5f3f744d3a49844c4a1093175b6fb
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2020-12-14 18:34:22 +0100

    send id, amount 4

--- app.py
+++ app.py
@@ -223,7 +223,7 @@ def customerForm():
         execute(query, False)
     except pymysql.err.IntegrityError:
         errortext = "Customer is already registered"
-        return render_template("customer.html", data = Amount, errortext = errortext)
+        return render_template("customer.html", data = data, errortext = errortext)
 
     CaID = valid_id()
     #sql_insert = "INSERT INTO D0018E.Cart (CaID, CuID) VALUES ({0}, (SELECT CID FROM D0018E.Customer WHERE CID = {1}))".format(CaID, request.cookies.get('SID'))
