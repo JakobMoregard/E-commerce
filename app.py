435acac24cb0c8b9d4bbdc166feef3d52826f5ea
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2020-12-15 17:32:59 +0100

    cart page 9

--- app.py
+++ app.py
@@ -285,10 +285,10 @@ def cart():
         data = request.args['data'].split(",")
         IID = valid_id()
 
-        sql_insert = "INSERT INTO D0018E.Item ({0}) VALUES ({1}, (SELECT CaID FROM D0018E.Cart WHERE CaID = {2}), (SELECT CaID FROM D0018E.Product WHERE PID = {3}), {4})".format(keys, str(IID), CaID, data[0], data[1])
+        sql_insert = "INSERT INTO D0018E.Item ({0}) VALUES ({1}, (SELECT CaID FROM D0018E.Cart WHERE CaID = {2}), (SELECT PID FROM D0018E.Product WHERE PID = {3}), {4})".format(keys, str(IID), CaID, data[0], data[1])
         execute(sql_insert, False)
 
-    sql_items = "SELECT IID, IAmount FROM D0018E.Item WHERE CaID = ".format(CaID)
+    sql_items = "SELECT IID, IAmount FROM D0018E.Item WHERE CaID = {}".format(CaID)
     table = execute(sql_items)
     return render_template("cart.html", table = x ,CartID = data)
 
