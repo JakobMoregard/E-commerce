7ecd50955accd609d3221f1a81fbbbe1dfb37b35
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2020-12-15 11:41:59 +0100

    error handling 7

--- app.py
+++ app.py
@@ -139,17 +139,17 @@ def cart_route():
 
     if request.cookies.get('login') == 'admin':
         return make_response(redirect("/"))
-    elif flag2:
-        print("flag2: ", request.form['Amount'])
-        if request.form['Amount'] == '' or request.form['Amount'] <= 0:
+    #elif flag2:
+    #    print("flag2: ", request.form['Amount'])
+    #    if request.form['Amount'] == '' or request.form['Amount'] <= 0:
+    #        return make_response(redirect("/"))
+    elif not flag2:
+        temp = data1.split(",")
+        print("temp: ", temp)
+        if temp == ['']: 
             return make_response(redirect("/"))
-    #elif not flag2:
-     #   temp = data1.split(",")
-     #   print("temp: ", temp)
-     #   if temp == ['']: 
-     #       return make_response(redirect("/"))
-     #   elif temp[1] <= 0:
-     #       return make_response(redirect("/"))
+        elif temp[1] <= 0:
+            return make_response(redirect("/"))
 
     sql1 = "SELECT CID from D0018E.Customer"
     customers = execute(sql1)
