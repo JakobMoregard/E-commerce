0e00bdb910a5fc206564cfc36edd154e80d51ccc
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2020-12-15 20:46:21 +0100

    cart page 31

--- app.py
+++ app.py
@@ -349,20 +349,20 @@ def change_cart():
     
     data = request.form
     print(data)
-    amount = int(data["Amount"])
-    sql = "SELECT IAmount FROM D0018E.Item WHERE IID = " + data['form_id']
-    cur_amount = execute(sql)
-    new_amount = amount + int(cur_amount[0]['IAmount'])
+    try:
+        amount = int(data["Amount"])
+        sql = "SELECT IAmount FROM D0018E.Item WHERE IID = " + data['form_id']
+        cur_amount = execute(sql)
+        new_amount = amount + int(cur_amount['IAmount'])
+    except ValueError:
+        return redirect("/cart")
 
-    if amount == '':
-        pass
-
-    elif new_amount == 0:
+    if new_amount == 0:
         pass #remove
     else:
         sql = "UPDATE D0018E.Item SET IAmount = {0} WHERE IID = {1}".format(new_amount, data['form_id'])
         execute(sql, False)
-        pass #update val
+
     
     return redirect("/cart")
 
