1572af2a1d04592622bb5d6c87d69624f41b710b
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2020-12-15 19:47:32 +0100

    cart page 19

--- app.py
+++ app.py
@@ -299,9 +299,9 @@ def cart():
             temp = str(int(data[1]) + int(PrIDs[0]['IAmount']))
             print(temp)
             update = "UPDATE D0018E.Item SET IAmount = {0} WHERE PrID = {1}".format(temp, PrIDs[0]['PrID'])
-
-        sql_insert = "INSERT INTO D0018E.Item ({0}) VALUES ({1}, (SELECT CaID FROM D0018E.Cart WHERE CaID = {2}), (SELECT PID FROM D0018E.Product WHERE PID = {3}), {4})".format(keys, str(IID), CaID, data[0], data[1])
-        execute(sql_insert, False)
+        else:
+            sql_insert = "INSERT INTO D0018E.Item ({0}) VALUES ({1}, (SELECT CaID FROM D0018E.Cart WHERE CaID = {2}), (SELECT PID FROM D0018E.Product WHERE PID = {3}), {4})".format(keys, str(IID), CaID, data[0], data[1])
+            execute(sql_insert, False)
 
     sql_items = "SELECT IID, IAmount FROM D0018E.Item WHERE CaID = {}".format(CaID)
     table = execute(sql_items)
