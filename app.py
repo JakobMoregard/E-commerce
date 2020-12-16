32ff62790ec664839952ec12fa7676810fd14f66
Jakob Moregard <jakob.moregard@gmail.com>
2020-12-16 17:21:08 +0100

    awsd

--- app.py
+++ app.py
@@ -527,32 +527,32 @@ def adminForm():
     elif req['form_id'] == '3':
 
         try:
-            sql = "DELETE FROM D0018E.Rating WHERE PrID = {};".format(req['PID'])
+            sql = "DELETE FROM D0018E.Rating WHERE PrID = (SELECT PrID FROM D0018E.Rating WHERE PrID = {});".format(req['PID'])
             execute(sql)
         except pymysql.err.Error:
             errortext = "Cannot remove from Rating"
             return render_template("admin.html", errortext = errortext, login = login_status(), loginstatus = request.cookies.get('login'))
 
         try:
-            sql = "DELETE FROM D0018E.Item WHERE PrID = {};".format(req['PID'])
+            sql = "DELETE FROM D0018E.Item WHERE PrID = (SELECT PrID FROM D0018E.Item WHERE PrID = {});".format(req['PID'])
             execute(sql)
         except pymysql.err.Error:
             errortext = "Cannot remove from Item"
             return render_template("admin.html", errortext = errortext, login = login_status(), loginstatus = request.cookies.get('login'))
 
         try:
-            sql = "DELETE FROM D0018E.Available WHERE PrID = {};".format(req['PID'])
+            sql = "DELETE FROM D0018E.Available WHERE PrID = (SELECT PrID FROM D0018E.Available WHERE PrID = {});".format(req['PID'])
             execute(sql)
         except pymysql.err.Error:
             errortext = "Cannot remove from Available"
             return render_template("admin.html", errortext = errortext, login = login_status(), loginstatus = request.cookies.get('login'))
 
-        #try:
-        sql = "DELETE FROM D0018E.Product WHERE PID = {};".format(req['PID'])
-        execute(sql)
-        #except pymysql.err.Error:
-            #errortext = "Cannot remove product"
-            #return render_template("admin.html", errortext = errortext, login = login_status(), loginstatus = request.cookies.get('login'))
+        try:
+            sql = "DELETE FROM D0018E.Product WHERE PID = {};".format(req['PID'])
+            execute(sql)
+        except pymysql.err.Error:
+            errortext = "Cannot remove product"
+            return render_template("admin.html", errortext = errortext, login = login_status(), loginstatus = request.cookies.get('login'))
 
 
     query3 = "SELECT AID, AFName, ALName, AMail FROM D0018E.Administrator WHERE AID = {}".format(request.cookies.get('SID'));
