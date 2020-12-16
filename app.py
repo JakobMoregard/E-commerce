0c886c475e69e21dfea420abde23b5f111357513
Jakob Moregard <jakob.moregard@gmail.com>
2020-12-16 18:14:14 +0100

    sda

--- app.py
+++ app.py
@@ -528,7 +528,7 @@ def adminForm():
         
         sql = "DELETE FROM D0018E.Product WHERE PID = {};".format(req['PID'])
         print(sql)
-        res = execute(sql)
+        res = execute(sql, False)
 
 
     query3 = "SELECT AID, AFName, ALName, AMail FROM D0018E.Administrator WHERE AID = {}".format(request.cookies.get('SID'));
