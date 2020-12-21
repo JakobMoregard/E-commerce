29b061bc9c009310974bb299b2c84f1471e162f9
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2020-12-21 14:51:04 +0100

    stock restrictions 14

--- app.py
+++ app.py
@@ -379,8 +379,11 @@ def cart():
         if PrIDs and str(PrIDs[0]['PrID']) in data:
             temp = int(data[1]) + int(PrIDs[0]['IAmount'])
             print(temp)
-            update = "UPDATE D0018E.Item SET IAmount = {0} WHERE IID = {1}".format(temp, int(PrIDs[0]['IID']))
-            execute(update, False)
+            if valid_amount(str(PrIDs[0]['PrID']), temp):
+                update = "UPDATE D0018E.Item SET IAmount = {0} WHERE IID = {1}".format(temp, int(PrIDs[0]['IID']))
+                execute(update, False)
+            else:
+                return render_template("cart.html", login = login_status(), loginstatus = request.cookies.get('login'))
         else:
             sql_insert = "INSERT INTO D0018E.Item ({0}) VALUES ({1}, (SELECT CaID FROM D0018E.Cart WHERE CaID = {2}), (SELECT PID FROM D0018E.Product WHERE PID = {3}), {4})".format(keys, str(IID), CaID, data[0], data[1])
             execute(sql_insert, False)
