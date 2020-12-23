a170806b713e99b1e4d494f053a2d852e0682520
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2020-12-23 15:26:44 +0100

    adding orders 6

--- app.py
+++ app.py
@@ -211,7 +211,7 @@ def cart_route():
         if not valid_amount(request.form['form_id'], int(request.form['Amount'])):
             return make_response(redirect("/"))
     else:
-        if not valid_amount(data[0], data[1]):
+        if not valid_amount(data1[0], data1[1]):
             return make_response(redirect("/"))
 
     if request.cookies.get('login') == 'admin':
