f8c391b9ed76e495db286207cc76f48713479d64
Jakob Moregard <jakob.moregard@gmail.com>
2020-12-30 13:56:44 +0100

    felsökning

--- app.py
+++ app.py
@@ -735,7 +735,7 @@ def userForm():
 def pp(): #you just had to
     product = parse_pid(request.args) #Finns troligen bättre metoder, oh well. Hey du fick det att funka iaf ¯\_(ツ)_/¯ I didn't....
 
-    sql = "SELECT Product.PName, Product.PColor, Product.PDescript, Available.APrice, Available.AStock FROM (D0018E.Product INNER JOIN D0018E.Available ON D0018E.Product.PID = D0018E.Available.PrID) WHERE PName = " + '"{}"'.format(product)
+    sql = "SELECT * FROM (D0018E.Product INNER JOIN D0018E.Available ON D0018E.Product.PID = D0018E.Available.PrID) WHERE PName = " + '"{}"'.format(product)
     print(sql)
     data = execute(sql)
 
