1c2e12e0c81c10e3c718de1decf41d41adb93b60
Jakob Moregard <jakob.moregard@gmail.com>
2021-01-03 14:57:43 +0100

    felsökning

--- app.py
+++ app.py
@@ -732,8 +732,10 @@ def userForm():
 
 @app.route("/ProductPage")
 def pp(): #you just had to smh...
+    print(request.args['data'])
     product = parse_pid(request.args['data']) #Finns troligen bättre metoder, oh well. Hey du fick det att funka iaf ¯\_(ツ)_/¯
     sql = "SELECT * FROM (D0018E.Product INNER JOIN D0018E.Available ON D0018E.Product.PID = D0018E.Available.PrID) WHERE PID = " + '"{}"'.format(product)
+    print(sql)
     product = execute(sql)
 
     return render_template("ProductPage.html", product = product, login = login_status(), loginstatus = request.cookies.get('login'))
