8ec2e7446f3aaaeccca90b56ee249810becd9b42
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2021-01-05 15:13:29 +0100

    changing cart and checkout tables 10

--- app.py
+++ app.py
@@ -506,7 +506,7 @@ def check_out():
         sql6 = "SELECT AStock FROM D0018E.Available WHERE PrID = (SELECT PrID FROM D0018E.Product WHERE PName = '{}' )".format(table[i]['PName'])
         available = execute(sql6)
         new_price = int(available[0]['AStock']) - int(table[i]['IAmount'])
-        sql7 = "UPDATE D0018E.Available SET AStock = {0} WHERE PrID = {1}".format(new_price ,table[i]['PrID'])
+        sql7 = "UPDATE D0018E.Available SET AStock = {0} WHERE PrID = (SELECT PrID FROM D0018E.Product WHERE PName = '{1}' )".format(new_price ,table[i]['PName'])
         execute(sql7, False)
 
     return render_template("check_out.html", table = table , old_tables = old_tableS, loginstatus = loginstatus)
