0738554d16b6bbd02e9cf503e85fc7ca8ad49bae
Simon Malmström Berghem <48282727+SimonBerghem@users.noreply.github.com>
2021-01-06 12:27:50 +0100

    reviews not working :/

--- app.py
+++ app.py
@@ -843,25 +843,30 @@ def review():
 @app.route("/Review", methods=['POST'])
 def write_review():
 
+    print("hm")
     data1 = request.args['data'].split(",")
     data1 = data1[0]
     print(data1)
     if request.cookies.get('login') == 'admin':
         return make_response(redirect(url_for('.pp', data = data1)))
 
+    print("ok")
     sql1 = "SELECT CID FROM D0018E.Customer"
     customers = execute(sql1)
 
     sql2 = "SELECT CuID, ReID FROM D0018E.Rating"
     IDs = execute(sql2)
+    print("kkk")
 
     cookie_id = int(request.cookies.get('SID'))
 
+    print("awda")
     flag = True
     for i in range(len(customers)):
         if customers[i]['CID'] == cookie_id:
             flag = False
     
+    print("YMCA")
     if request.cookies.get('login') == 'None' and flag:
         res = make_response(redirect(url_for(".customer", data = data1)))
         return res
@@ -869,6 +874,7 @@ def write_review():
     res = make_response(redirect(url_for('.pp', data = data1)))
     RaID = valid_id()
     
+    print("well")
     if request.cookies.get('login') == 'registered':
         sql_insert = "INSERT INTO D0018E.Rating (RaID, RRating, RReview, PrID, ReID) VALUES({0}, {1}, '{2}', (SELECT PID FROM D0018E.Product WHERE PID = {3}), (SELECT RID FROM D0018E.Registered WHERE RID = {4}));".format(RaID, request.form['RRating'], request.form['RReview'], data1, cookie_id)
         print(sql_insert)
