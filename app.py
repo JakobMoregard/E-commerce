98edbee9a4a33f93a133352dc1f6536b0ac44c2e
Jakob Moregard <jakob.moregard@gmail.com>
2021-01-06 21:57:13 +0100

    test 3

--- app.py
+++ app.py
@@ -387,7 +387,7 @@ def cart():
             res = execute(sql1)
             CaID = res[0]['CaID']
     except IndexError:
-        return render_template("cart.html", empty_flag = True, login = login_status(), loginstatus = request.cookies.get('login'))
+        return render_template("cart.html", empty_flag, login = login_status(), loginstatus = request.cookies.get('login'))
     print(CaID)
     
     #data[0] = PID, data[1] = Amount, data[2] = price
